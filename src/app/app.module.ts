892fd30f2e5e0e83642006f7dbfa16778bf2a436
VincenzoChiodi <vincenzo.chiodi@gmail.com>
2025-04-18 15:27:29 +0200

    app module e user-imput model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { InvestmentResultComponent } from "./investment/investment-result/investment-result.component";
-import { UserInputComponent } from "./investment/user-input/user-input.component";
-import { FormsModule } from "@angular/forms";
+
+import { UserInputModule } from "./investment/user-input/user-imput.module"; 
 import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-    declarations:[AppComponent,HeaderComponent,InvestmentResultComponent,UserInputComponent],
-    imports:[FormsModule,BrowserModule],
+    declarations:[AppComponent,HeaderComponent,InvestmentResultComponent],
+    imports:[UserInputModule,BrowserModule],
     bootstrap:[AppComponent]
 })
 
